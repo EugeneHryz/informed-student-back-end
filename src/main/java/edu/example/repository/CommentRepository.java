426f072f3ff3da8c6f48ebc5f0241ec625e5424e
Eugene <evgeniy.hryz@gmail.com>
2023-12-12 12:49:49 +0300

    get comments without their replies

--- src/main/java/edu/example/repository/CommentRepository.java
+++ src/main/java/edu/example/repository/CommentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.history.RevisionRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +17,9 @@ public interface CommentRepository extends JpaRepository<Comment, Long>, Revisio
 
     List<Comment> findCommentsByPostOrderByCreatedAt(Post post);
 
+    @Query(value = "select c from Comment c " +
+            "left join CommentReply cr on cr.reply = c " +
+            "where cr is null and c.post.id = :postId")
     @EntityGraph(attributePaths = {"user"})
     Page<Comment> findByPostId(Long postId, Pageable pageable);
 
